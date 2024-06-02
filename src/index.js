2c9e6cc55d141a0824116bae5cdc345279977c5f
Olaoluwa Oyebode <olaolumcpaul@gmail.com>
2024-06-02 21:58:33 +0100

    Olaoluwa Oyebode_Pull Request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
-
 console.log(data);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+const cardsList = document.querySelector(".cards");
+
+for (let i = 0; i < data.length; i++) {
+  const pokemon = data[i];
+
+  const card = document.createElement("li");
+  card.classList.add("card");
+
+  const pokemonName = document.createElement("h2");
+  pokemonName.classList.add("card--text");
+  pokemonName.textContent = pokemon.name;
+  card.appendChild(pokemonName);
+
+  const pokemonImage = document.createElement("img");
+  pokemonImage.src = pokemon.sprites.other["official-artwork"].front_default;
+  pokemonImage.width = 256;
+  pokemonImage.height = 256;
+  card.appendChild(pokemonImage);
+
+  const pokemonStats = document.createElement("ul");
+  pokemonStats.classList.add("card--text");
+  pokemonStats.textContent = "";
+  for (let j = 0; j < pokemon.stats.length; j++) {
+    const stat = pokemon.stats[j];
+    const statItem = document.createElement("li");
+    statItem.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+    pokemonStats.appendChild(statItem);
+  }
+  card.appendChild(pokemonStats);
+
+  cardsList.appendChild(card);
+}
+
+// remove bullets from the card list
+const list = document.querySelector(".cards");
+list.style.listStyle = "none";
+
+// center the image
+const images = document.querySelectorAll("img");
+for (let i = 0; i < images.length; i++) {
+  images[i].style.display = "block";
+  images[i].style.margin = "auto";
+  images[i].backgroundColor = "lightgrey";
+}
